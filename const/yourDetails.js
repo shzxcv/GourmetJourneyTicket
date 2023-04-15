e24138d878f288d9baec4c507df9d037a67fad11
sho-jp <sh.zxcv00@gmail.com>
2023-04-16 02:55:16 +0900

    add const files

---
+++ const/yourDetails.js
@@ -0,0 +1,11 @@
+// Replace this with your contract address
+export const contractAddress = "0x2d851ee8C7804119fC0a5807a5b8619078F32d32";
+
+// Replace this with your domain name.
+export const domainName = "example.com";
+
+// Minimum number of tokens required to access
+export const minimumBalance = 1;
+
+// Token ID for ERC-1155 tokens
+export const erc1155TokenId = 0;
